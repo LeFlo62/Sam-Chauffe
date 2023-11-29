3646c492ef574e27b73f4c4608d0258e3646ee2f
azertysarah <92745305+azertysarah@users.noreply.github.com>
2023-11-29 14:15:57 +0100

    Updated scripts & prefab

--- Assets/Scripts/ObjectSpawnController.cs
+++ Assets/Scripts/ObjectSpawnController.cs
@@ -2,64 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawnController : MonoBehaviour
+namespace SamChauffe
 {
-    public GameObject objectPrefab;
-
-    public int spawnAmount = 1;
-    
-    public Transform[] spawnPoints;
-
-    private void OnDrawGizmos()
+    public class ObjectSpawnController : MonoBehaviour
     {
-        if(objectPrefab != null)
+        public GameObject objectPrefab;
+        public int minSpawnAmount = 1;
+        public int maxSpawnAmount = 1;
+
+
+        public Transform[] spawnPoints;
+
+        private void OnDrawGizmos()
         {
-            Gizmos.color = objectPrefab.GetComponent<Renderer>().sharedMaterial.color;
-            foreach (Transform spawnPoint in spawnPoints)
+            if (objectPrefab != null)
             {
-                Gizmos.DrawWireMesh(objectPrefab.GetComponent<MeshFilter>().sharedMesh, spawnPoint.position, spawnPoint.rotation, spawnPoint.localScale);
+                Gizmos.color = objectPrefab.GetComponent<Renderer>().sharedMaterial.color;
+                foreach (Transform spawnPoint in spawnPoints)
+                {
+                    Gizmos.DrawWireMesh(objectPrefab.GetComponent<MeshFilter>().sharedMesh, spawnPoint.position, spawnPoint.rotation, spawnPoint.localScale);
+                }
             }
         }
-    }
 
-    private void Start()
-    {
-        if(spawnAmount > spawnPoints.Length)
+        private void Start()
         {
-            spawnAmount = spawnPoints.Length;
-        }
-        
-        int[] indexes = RandomArray(spawnPoints.Length);
+            int spawnAmount = Random.Range(minSpawnAmount, maxSpawnAmount);
 
-        for (int i = 0; i < spawnAmount; ++i)
-        {
-            Instantiate(objectPrefab, spawnPoints[indexes[i]].position, spawnPoints[indexes[i]].rotation, spawnPoints[indexes[i]]);
-        }
-    }
+            if (spawnAmount > spawnPoints.Length)
+            {
+                spawnAmount = spawnPoints.Length;
+            }
 
-    //
-    // Summary:
-    //     Returns a shuffled array of indexes from 0 to length - 1
-    //
-    // Parameters:
-    //   length:
-    //     The length of the array
-    private int[] RandomArray(int length)
-    {
-        int[] array = new int[length];
-        for (int i = 0; i < length; ++i)
-        {
-            array[i] = i;
+            int[] indexes = RandomArray(spawnPoints.Length);
+
+            for (int i = 0; i < spawnAmount; ++i)
+            {
+                Instantiate(objectPrefab, spawnPoints[indexes[i]].position, spawnPoints[indexes[i]].rotation, spawnPoints[indexes[i]]);
+            }
         }
 
-        for (int i = 0; i < length; ++i)
+        //
+        // Summary:
+        //     Returns a shuffled array of indexes from 0 to length - 1
+        //
+        // Parameters:
+        //   length:
+        //     The length of the array
+        private int[] RandomArray(int length)
         {
-            int temp = array[i];
-            int randomIndex = Random.Range(i, length);
-            array[i] = array[randomIndex];
-            array[randomIndex] = temp;
-        }
+            int[] array = new int[length];
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = i;
+            }
 
-        return array;
+            for (int i = 0; i < length; ++i)
+            {
+                int temp = array[i];
+                int randomIndex = Random.Range(i, length);
+                array[i] = array[randomIndex];
+                array[randomIndex] = temp;
+            }
+
+            return array;
+        }
     }
 }
